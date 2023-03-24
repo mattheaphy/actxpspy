2152e2833e3a5db1b273e720b6e34097f01868c8
Matt Heaphy <matt.heaphy@gmail.com>
2023-03-24 00:38:19 -0400

    added toy census data

--- actxpspy/__init__.py
+++ actxpspy/__init__.py
@@ -1,1 +1,2 @@
 from actxpspy.pol_yr import *
+from actxpspy.datasets import *
---
+++ actxpspy/datasets.py
@@ -0,0 +1,26 @@
+import pkg_resources
+import pandas as pd
+
+def load_toy_census() -> pd.DataFrame:
+    """
+    # Toy policy census data
+    
+    A tiny dataset containing 3 policies: one active, one terminated due to
+    death, and one terminated due to surrender.    
+
+    ## Returns:
+    
+    A data frame with 3 rows and 4 columns:
+    
+    - `pol_num` = policy number
+    - `status` = policy status
+    - `issue_date` = issue date
+    - `term_date` = termination date
+    
+    """
+    stream = pkg_resources.resource_stream(__name__, 'data/toy_census.csv')
+    return pd.read_csv(stream,
+                       index_col=0,
+                       dtype={'pol_num': str,
+                              'status': 'category'},
+                       parse_dates=['issue_date', 'term_date'])

345e40baa93b96d5779199d3c42537614f5bd1b7
Matt Heaphy <matt.heaphy@gmail.com>
2023-03-25 08:02:56 -0400

    argument matching function

--- actxps/tools.py
+++ actxps/tools.py
@@ -1,6 +1,6 @@
 # This module contains helper functions used by other modules
 
-def document(docstring):
+def document(docstring: str):
     """
     Decorator function factory for adding docstrings
 
@@ -19,3 +19,25 @@ def document(docstring):
         return func
 
     return decorator
+
+
+def arg_match(name: str, x, allowed):
+    """
+    Verify that an argument contains one of several allowed values.
+
+    A `ValueError` exception is raised if the argument value `x` is not allowed.
+
+    ## Parameters
+
+    `name`: str
+        Argument name
+    `x`: Any
+        Argument value
+    `allowed`: Any
+        A list of allowed argument values
+    """
+    if x not in allowed:
+        allowed = ", ".join([f'"{a}"' for a in allowed[:-1]]) + \
+            f', or "{allowed[-1]}"'
+        raise ValueError(f'`{name}` must be one of {allowed}. '
+                         f'"{x}" is not allowed.')

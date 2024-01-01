831bd1ac14fab6a0960a06bb1b2174f8fafba715
Matt Heaphy <matt.heaphy@gmail.com>
2024-01-01 09:29:20 -0500

    test update for great_tables

--- tests/test_tables.py
+++ tests/test_tables.py
@@ -2,6 +2,7 @@ from actxps import ExposedDF
 import actxps as xp
 import numpy as np
 import pandas as pd
+from great_tables import GT
 
 census_dat = xp.load_census_dat()
 
@@ -14,37 +15,34 @@ expo.groupby('pol_yr', 'inc_guar')
 exp_res = expo.exp_stats()
 exp_res2 = expo.exp_stats(wt="premium", credibility=True,
                           expected="q_exp")
-_ = exp_res.table()
 
 trx_res = expo.trx_stats()
 trx_res2 = expo.trx_stats(percent_of='premium')
-
-styler = pd.io.formats.style.Styler
 
 
 class TestAutotable():
 
     def test_table_basic(self):
-        assert isinstance(exp_res.table(), styler)
+        assert isinstance(exp_res.table(), GT)
 
     def test_table_options(self):
-        assert isinstance(exp_res2.table(), styler)
+        assert isinstance(exp_res2.table(), GT)
 
     def test_table_renames(self):
         assert isinstance(exp_res2.table(
-            rename_cols={'pol_yr': 'Policy year',
-                         'inc_guar': 'GLWB'}), styler)
+            pol_yr='Policy year',
+            inc_guar='GLWB'), GT)
 
 
 class TestTrxAutotable():
 
     def test_table_basic(self):
-        assert isinstance(trx_res.table(), styler)
+        assert isinstance(trx_res.table(), GT)
 
     def test_table_options(self):
-        assert isinstance(trx_res2.table(), styler)
+        assert isinstance(trx_res2.table(), GT)
 
     def test_table_renames(self):
         assert isinstance(trx_res2.table(
-            rename_cols={'pol_yr': 'Policy year',
-                         'inc_guar': 'GLWB'}), styler)
+            pol_yr='Policy year',
+            inc_guar='GLWB'), GT)
